e94ca82d640d72a89155b9e7c172dd5b22480a02
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2023-01-17 21:35:26 +0300

    12

--- 01.py
+++ 01.py
@@ -1,7 +1,7 @@
 #Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-f = open('123.txt')
-some_list = f.read()
+with open ('123.txt') as f:
+    some_list = f.read()
 new_list = filter(lambda word: 'abc' not in word, some_list)
 print(list(new_list))
 
